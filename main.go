9333e3389ac3766dcd7992b88e3dad4f9e47b178
Julio Davila <juliodacer@gmail.com>
2025-09-07 11:20:32 -0500

    conditions

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/juliodacer/GoDesdeCero/variables"
 )
@@ -10,4 +11,19 @@ func main() {
 	state, text := variables.ConvertToText(2348)
 	fmt.Println(state)
 	fmt.Println(text)
+
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("Esto no es Windows, es ", os)
+	} else {
+		fmt.Println("Esto es Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
